c8b19e8e534ce46723018f212d93bd57114efc62
Lalit Singh <132752354+ltlalit15@users.noreply.github.com>
2025-05-31 11:40:20 +0530

    Update dbConnect.js

--- Config/dbConnect.js
+++ Config/dbConnect.js
@@ -1,44 +1,44 @@
 import { JSONCookie } from 'cookie-parser';
 import { query } from 'express';
 import mysql from 'mysql2/promise';
-export const pool =  mysql.createPool({
-  host: 'localhost',
-  port:3306,
-  user: 'root',
-  database: 'samrtlife',
-});
-(async () => {
-    try {
-      const connection = await pool.getConnection();
-      console.log("✅ Successfully connected Local MySQL database");
-      connection.release();
-    } catch (error) {
-      console.error("❌ Database connection error:", error);
-    }
-})();
+//export const pool =  mysql.createPool({
+//  host: 'localhost',
+//  port:3306,
+//  user: 'root',
+//  database: 'samrtlife',
+//});
+//(async () => {
+//    try {
+//      const connection = await pool.getConnection();
+//      console.log("✅ Successfully connected Local MySQL database");
+//      connection.release();
+//    } catch (error) {
+//      console.error("❌ Database connection error:", error);
+//    }
+//})();
 
-// export const pool = mysql.createPool({
-//   host: "turntable.proxy.rlwy.net",            // ✅ Updated host
-//   port: 24563,                                // ✅ Updated port
-//   user: "root",                               // ✅ Username
-//   password: "NOLyTehfdCBmfOWVmasiqTBHheJvCjNN", // ✅ New password
-//   database: "railway",                        // ✅ Database name
-//   waitForConnections: true,
-//   connectionLimit: 10,
-//   queueLimit: 0,
-//   connectTimeout: 10000, // 10 seconds timeout
-// });
+ export const pool = mysql.createPool({
+   host: "ballast.proxy.rlwy.net",            // ✅ Updated host
+   port: 20416,                                // ✅ Updated port
+   user: "root",                               // ✅ Username
+   password: "GzTjhUpRLgsgASPPaJrkkyGKqIyJQVFp", // ✅ New password
+   database: "railway",                        // ✅ Database name
+   waitForConnections: true,
+   connectionLimit: 10,
+   queueLimit: 0,
+   connectTimeout: 10000, // 10 seconds timeout
+ });
 
-// // ✅ Check Connection
-// (async () => {
-//   try {
-//     const connection = await pool.getConnection();
-//     console.log("✅ Successfully connected to Railway MySQL database");
-//     connection.release();
-//   } catch (error) {
-//     console.error("❌ Database connection error:", error);
-//   }
-// })();
+ // ✅ Check Connection
+ (async () => {
+   try {
+     const connection = await pool.getConnection();
+     console.log("✅ Successfully connected to Railway MySQL database");
+     connection.release();
+   } catch (error) {
+     console.error("❌ Database connection error:", error);
+   }
+ })();
 
 
 
